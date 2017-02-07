7b136ae0e24a996e2928034c46aa6e5289a6a4e1
Jorick Sijtsma <j.o.sijtsma@st.hanze.nl>
2017-02-07 16:19:54 +0100

    alle logic toegevoegd en verwerkt

---
+++ ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/logic/NormalCar.java
@@ -0,0 +1,20 @@
+package nl.PriorIT.src.ParkingSimulator.logic;
+
+import java.util.Random;
+import java.awt.*;
+
+public class NormalCar extends Car {
+
+	private static final Color COLOR=Color.red;
+	
+    public NormalCar() {
+    	Random random = new Random();
+    	int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
+        this.setMinutesLeft(stayMinutes);
+        this.setHasToPay(true);
+    }
+    
+    public Color getColor(){
+    	return COLOR;
+    }
+}
---
+++ ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/view/CarParkView.java
@@ -0,0 +1,69 @@
+package nl.PriorIT.src.ParkingSimulator.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import nl.PriorIT.src.ParkingSimulator.logic.*;
+
+import javax.swing.JPanel;
+
+public class CarParkView extends JPanel {
+        
+        private Dimension size;
+        private Image carParkImage;
+        
+        private CarParkView carParkView;
+        private int numberOfFloors;
+        private int numberOfRows;
+        private int numberOfPlaces;
+        private int numberOfOpenSpots;
+        private Car[][][] cars;
+    
+        /**
+         * Constructor for objects of class CarPark
+         */
+        public CarParkView() {
+            size = new Dimension(0, 0);
+        }
+    
+        /**
+         * Overridden. Tell the GUI manager how big we would like to be.
+         */
+        public Dimension getPreferredSize() {
+            return new Dimension(800, 500);
+        }
+    
+        /**
+         * Overriden. The car park view component needs to be redisplayed. Copy the
+         * internal image to screen.
+         */
+        public void paintComponent(Graphics g) {
+            if (carParkImage == null) {
+                return;
+            }
+    
+            Dimension currentSize = getSize();
+            if (size.equals(currentSize)) {
+                g.drawImage(carParkImage, 0, 0, null);
+            }
+            else {
+                // Rescale the previous image.
+                g.drawImage(carParkImage, 0, 0, currentSize.width, currentSize.height, null);
+            }
+        }
+    
+        /**
+         * Paint a place on this car park view in a given color.
+         */
+        public static void drawPlace(Graphics graphics, Location location, Color color) {
+            graphics.setColor(color);
+            graphics.fillRect(
+                    location.getFloor() * 260 + (1 + (int)Math.floor(location.getRow() * 0.5)) * 75 + (location.getRow() % 2) * 20,
+                    60 + location.getPlace() * 10,
+                    20 - 1,
+                    10 - 1); // TODO use dynamic size or constants
+        }
+    }
+
+
