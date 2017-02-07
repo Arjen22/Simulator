4b32606f2edb521c9e9739c0f06cc6dc0a7c8348
Jorick Sijtsma <j.o.sijtsma@st.hanze.nl>
2017-02-07 14:21:58 +0100

    model aangemaakt

---
+++ ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/logic/Model.java
@@ -0,0 +1,11 @@
+/**
+ * @author PriorIT
+ * @version 0.1 DayBuild 1
+ * The abstract model contains all the common classes of all the models.
+ */
+package nl.PriorIT.src.ParkingSimulator.logic;
+
+
+public abstract class Model {
+
+}
