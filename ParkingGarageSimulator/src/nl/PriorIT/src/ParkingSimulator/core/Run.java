8bb3a0350fac557a1b4de87312b20ecf556a518a
Jorick Sijtsma <j.o.sijtsma@st.hanze.nl>
2017-02-08 15:37:56 +0100

    run toegevoegd

---
+++ ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/core/Run.java
@@ -0,0 +1,15 @@
+package nl.PriorIT.src.ParkingSimulator.core;
+
+
+public class Run {
+
+    /**
+     * @param args
+     */
+	
+    public static void main(String[] args) {
+		new Main();
+	}
+    
+
+}
