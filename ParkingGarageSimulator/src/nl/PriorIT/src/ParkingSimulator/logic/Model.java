822004f506220d292c738984b1714185cdb12c54
Jorick Sijtsma <j.o.sijtsma@st.hanze.nl>
2017-02-09 21:12:28 +0100

    abboplekken nog niet helemaal goed maar het werkt wel ongeveer.

--- ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/logic/Model.java
+++ ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/logic/Model.java
@@ -22,11 +22,12 @@ public class Model extends GeneralModel {
 	private int numberOfFloors;
 	private int numberOfRows;
 	private int numberOfPlaces;
-	private static int abboplekken;
 	private int numberOfOpenSpots;
+	private int numberOfSpots;
 	private Car[][][] cars;
 	private Location lastplace;
-	private static int abboplek;
+	private static int abboplekken;
+	Random random = new Random();
 
 	private int day = 0;
 	private int hour = 0;
@@ -51,152 +52,170 @@ public class Model extends GeneralModel {
 		this.numberOfRows = numberOfRows;
 		this.numberOfPlaces = numberOfPlaces;
 		this.numberOfOpenSpots =numberOfFloors*numberOfRows*numberOfPlaces;
+		numberOfSpots = numberOfOpenSpots;
+		this.abboplekken = abboplekken;
 		cars = new Car[numberOfFloors][numberOfRows][numberOfPlaces];
 		
 		entranceCarQueue = new CarQueue();
 	    entrancePassQueue = new CarQueue();
 	    paymentCarQueue = new CarQueue();
 	    exitCarQueue = new CarQueue();
-	    lastplace = new Location(0,2,6);
+	    lastplace = lastloc();
 	    
 	}
 
 	public static int getAbboPlekken(){
-    	abboplek = abboplekken;
     	return abboplekken;
     }
 	
-public void handleEntrance(){
-	carsArriving();
-	carsEntering(entrancePassQueue);
-	carsEntering(entranceCarQueue);  	
-}
-
-public void run(Model simulatormodel, SimulatorController controller, CarParkView carparkview) {
-    notifyViews();
-    for (int i = 0; i < 1000000; i++) {
-        tick(carparkview);
-    }
-}
-
-public void handleExit(){
-    carsReadyToLeave();
-    carsPaying();
-    carsLeaving();
-}
-
-public void advanceTime(){
-    // Advance the time by one minute.
-    minute++;
-    while (minute > 59) {
-        minute -= 60;
-        hour++;
-    }
-    while (hour > 23) {
-        hour -= 24;
-        day++;
-    }
-    while (day > 6) {
-        day -= 7;
-    }
-}
-
-public void updateViews(CarParkView carparkview){
-   this.carparkview = carparkview;
-   carparkview.tick();
-    // Update the car park view.
-   carparkview.updateView();	
-}
-
-private void carsArriving(){
-	int numberOfCars=getNumberOfCars(weekDayArrivals, weekendArrivals);
-    addArrivingCars(numberOfCars, NORMCAR);    	
-	numberOfCars=getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
-    addArrivingCars(numberOfCars, PASS);    	
-}
-
-private void carsEntering(CarQueue queue){
-    int i=0;
-    // Remove car from the front of the queue and assign to a parking space.
-	while (queue.carsInQueue()>0 && 
-			getNumberOfOpenSpots()>0 && 
-			i<enterSpeed) {
-        Car car = queue.removeCar();
-        Location freeLocation = getFirstFreeLocation(car.getColor());
-        setCarAt(freeLocation, car);
-        i++;
-    }
-}
-
-private void carsReadyToLeave(){
-    // Add leaving cars to the payment queue.
-    Car car = getFirstLeavingCar();
-    while (car!=null) {
-    	if (car.getHasToPay()){
-            car.setIsPaying(true);
-            paymentCarQueue.addCar(car);
-    	}
-    	else {
-    		carLeavesSpot(car);
-    	}
-        car = getFirstLeavingCar();
-    }
-}
-
-private void carsPaying(){
-    // Let cars pay.
-	int i=0;
-	while (paymentCarQueue.carsInQueue()>0 && i < paymentSpeed){
-        Car car = paymentCarQueue.removeCar();
-        // TODO Handle payment.
-        carLeavesSpot(car);
-        i++;
+	public void handleEntrance(){
+		carsArriving();
+		carsEntering(entrancePassQueue);
+		carsEntering(entranceCarQueue);  	
 	}
-}
 
-private void carsLeaving(){
-    // Let cars leave.
-	int i=0;
-	while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
-        exitCarQueue.removeCar();
-        i++;
-	}	
-}
-
-private int getNumberOfCars(int weekDay, int weekend){
-    Random random = new Random();
-
-    // Get the average number of cars that arrive per hour.
-    int averageNumberOfCarsPerHour = day < 5
-            ? weekDay
-            : weekend;
-
-    // Calculate the number of cars that arrive this minute.
-    double standardDeviation = averageNumberOfCarsPerHour * 0.3;
-    double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
-    return (int)Math.round(numberOfCarsPerHour / 60);	
-}
-
-private void addArrivingCars(int numberOfCars, String type){
-    // Add the cars to the back of the queue.
-	switch(type) {
-	case NORMCAR: 
-        for (int i = 0; i < numberOfCars; i++) {
-        	entranceCarQueue.addCar(new NormalCar());
-        }
-        break;
-	case PASS:
-        for (int i = 0; i < numberOfCars; i++) {
-        	entrancePassQueue.addCar(new ParkingPassCar());
-        }
-        break;	            
+	public void run(Model simulatormodel, SimulatorController controller, CarParkView carparkview) {
+		notifyViews();
+		for (int i = 0; i < 1000000; i++) {
+			tick(carparkview);
+		}
 	}
-}
+	
+	public Location lastloc() {
+		int floor = 0;
+		int row = 0;
+		int place = abboplekken;
+		for(; place > numberOfPlaces; place -= numberOfPlaces) {
+			row++;
+		}
+		for (; row > numberOfRows; row -= numberOfRows) {
+			floor++;
+		}
+		Location location = new Location(floor, row , place);
+		return location;
+	}
 
-private void carLeavesSpot(Car car){
-	removeCarAt(car.getLocation());
-    exitCarQueue.addCar(car);
-}
+	public void handleExit(){
+		carsReadyToLeave();
+		carsPaying();
+		carsLeaving();
+	}
+
+	public void advanceTime(){
+		// Advance the time by one minute.
+		minute++;
+		while (minute > 59) {
+			minute -= 60;
+			hour++;
+		}
+		while (hour > 23) {
+			hour -= 24;
+			day++;
+		}
+		while (day > 6) {
+			day -= 7;
+		}
+	}
+
+	public void updateViews(CarParkView carparkview){
+		this.carparkview = carparkview;
+		carparkview.tick();
+		// Update the car park view.
+		carparkview.updateView();	
+	}
+
+	private void carsArriving(){
+		int numberOfCars=getNumberOfCars(weekDayArrivals, weekendArrivals);
+		addArrivingCars(numberOfCars, NORMCAR);    	
+		numberOfCars=getNumberOfCars(weekDayPassArrivals, weekendPassArrivals);
+		addArrivingCars(numberOfCars, PASS);    	
+	}
+
+	private void carsEntering(CarQueue queue){
+		int i=0;
+		// Remove car from the front of the queue and assign to a parking space.
+		while (queue.carsInQueue()>0 && 
+				getNumberOfOpenSpots()>0 && 
+				i<enterSpeed) {
+			Car car = queue.removeCar();
+			Location freeLocation = getFirstFreeLocation(car.getColor());
+			if (freeLocation == null) {
+				System.out.println("godwhy");
+			}
+			setCarAt(freeLocation, car);
+			i++;
+		}
+	}
+
+	private void carsReadyToLeave(){
+		// Add leaving cars to the payment queue.
+		Car car = getFirstLeavingCar();
+		while (car!=null) {
+			if (car.getHasToPay()){
+				car.setIsPaying(true);
+				paymentCarQueue.addCar(car);
+			}
+			else {
+				carLeavesSpot(car);
+			}
+			car = getFirstLeavingCar();
+		}
+	}
+
+	private void carsPaying(){
+		// Let cars pay.
+		int i=0;
+		while (paymentCarQueue.carsInQueue()>0 && i < paymentSpeed){
+			Car car = paymentCarQueue.removeCar();
+			// TODO Handle payment.
+			carLeavesSpot(car);
+			i++;
+		}
+	}
+
+	private void carsLeaving(){
+		// Let cars leave.
+		int i=0;
+		while (exitCarQueue.carsInQueue()>0 && i < exitSpeed){
+			exitCarQueue.removeCar();
+			i++;
+		}	
+	}
+
+	private int getNumberOfCars(int weekDay, int weekend){
+		Random random = new Random();
+		
+		// Get the average number of cars that arrive per hour.
+		int averageNumberOfCarsPerHour = day < 5
+				? weekDay
+						: weekend;
+		
+		// Calculate the number of cars that arrive this minute.
+		double standardDeviation = averageNumberOfCarsPerHour * 0.3;
+		double numberOfCarsPerHour = averageNumberOfCarsPerHour + random.nextGaussian() * standardDeviation;
+		return (int)Math.round(numberOfCarsPerHour / 60);	
+	}
+
+	private void addArrivingCars(int numberOfCars, String type){
+		// Add the cars to the back of the queue.
+		switch(type) {
+		case NORMCAR: 
+			for (int i = 0; i < numberOfCars; i++) {
+				entranceCarQueue.addCar(new NormalCar());
+			}
+			break;
+		case PASS:
+			for (int i = 0; i < numberOfCars; i++) {
+				entrancePassQueue.addCar(new ParkingPassCar());
+			}
+			break;	            
+		}
+	}
+
+	private void carLeavesSpot(Car car){
+		removeCarAt(car.getLocation());
+		exitCarQueue.addCar(car);
+	}
     
 	public int getNumberOfFloors() {
         return numberOfFloors;
@@ -249,27 +268,71 @@ private void carLeavesSpot(Car car){
         numberOfOpenSpots++;
         return car;
     }
-
-    public Location getFirstFreeLocation(Color color) {
-    	boolean firstime = true;
-        for (int floor = 0; floor < getNumberOfFloors(); floor++) {
+    
+    public int getFullGarage(boolean penis) {
+    	int baby = 0;
+    	boolean firsttime = penis;
+    	
+    	condom:
+    	for (int floor = 0; floor < getNumberOfFloors(); floor++) {
             for (int row = 0; row < getNumberOfRows(); row++) {
                 for (int place = 0; place < getNumberOfPlaces(); place++) {
-                    if(color != Color.blue && firstime == true) {
-                    	floor = lastplace.getFloor();
-                    	row = lastplace.getRow();
-                    	place = lastplace.getPlace();
-                    	firstime = false;
-                    }
-                    
-                	Location location = new Location(floor, row, place);
-                    if (getCarAt(location) == null) {
-                        return location;
-                    }
-                }
-            }
-        }
-        return null;
+                	if(firsttime == false) {
+                		floor = lastplace.getFloor();
+                		row = lastplace.getRow();
+                		place = lastplace.getPlace();
+                		firsttime = true;
+                	} //telt niet goed
+                	
+            		Location vagina = new Location(floor, row, place);
+            		
+                	if (getCarAt(vagina) != null) {
+                		baby++;
+                	}
+                	
+                	if (penis) { //als penis false is dan telt die niet goed
+                		if(floor == lastplace.getFloor() && row == lastplace.getRow() && place == lastplace.getPlace()) {
+                			break condom;
+                		}
+                	}
+    			}
+    		}
+    	}
+    	return baby;
+    }
+
+    public Location getFirstFreeLocation(Color color) {
+    	Location location = null;
+    	int floor;
+    	int row;
+    	int place;
+    	boolean full = getFullGarage(true) == abboplekken ? true : false;
+    	while (color == Color.blue && full == false && location == null) {
+    		floor = random.nextInt(numberOfFloors);
+    		floor = floor <= lastplace.getFloor() ? floor : 899;
+    		row = floor < lastplace.getFloor() ?
+    				random.nextInt(numberOfRows) : random.nextInt(lastplace.getRow()+1);
+    		place = row  == lastplace.getRow() && floor == lastplace.getFloor() ?
+    				random.nextInt(lastplace.getPlace()) : random.nextInt(numberOfPlaces);
+    		
+        	if (floor != 899) {
+        		location = new Location(floor, row, place);
+        		location = getCarAt(location) == null ? location : null;
+        	}
+    	}
+    	
+    	full = getFullGarage(false) == (numberOfSpots - abboplekken) ? true : false;
+    	while(full == false && location == null) {
+    		floor = random.nextInt(numberOfFloors - lastplace.getFloor()) + lastplace.getFloor();
+    		row = floor == lastplace.getFloor() ?
+    				random.nextInt(numberOfRows - lastplace.getRow()) + lastplace.getRow() : random.nextInt(numberOfFloors) ;
+    		place = row == lastplace.getRow() && floor == lastplace.getFloor() ?
+    				random.nextInt(numberOfPlaces - lastplace.getPlace()) + lastplace.getPlace() : random.nextInt(numberOfPlaces);
+    		//bovenstaande is ook niet goed
+    		location = new Location(floor, row, place);
+    		location = getCarAt(location) == null ? location : null;
+    	}
+    	return location;
     }
 
     public Car getFirstLeavingCar() {
@@ -311,7 +374,3 @@ private void carLeavesSpot(Car car){
         return true;
     }
 }
-
-
-
-
