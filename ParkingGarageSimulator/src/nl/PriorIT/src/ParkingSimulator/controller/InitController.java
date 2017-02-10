842753b1842bd756989a289069c93efafa9e6041
Jos Terlage <Jos211@live.nl>
2017-02-10 11:33:05 +0100

    Made the actionevent for the button work so it sets the variables

--- ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/controller/InitController.java
+++ ParkingGarageSimulator/src/nl/PriorIT/src/ParkingSimulator/controller/InitController.java
@@ -7,26 +7,33 @@ import nl.PriorIT.src.ParkingSimulator.logic.Model;
 
 public class InitController extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 8084081366423909672L;
-	private JTextField size;
-	private JTextField degree;
+	private JTextField floors;
+	private JTextField rows;
+	private JTextField places;
+	private JTextField abboplekken;
 	private JButton init;
-	
 	
 	public InitController(Model parkingSimulator) {
 		//super(parkingSimulator);
 		setSize(200, 300);
-		size=new JTextField();
-		degree=new JTextField();
+		floors=new JTextField();
+		rows=new JTextField();
+		places = new JTextField();
+		abboplekken = new JTextField();
 		init=new JButton("Init");
 		init.addActionListener(this);
 		
 		this.setLayout(null);
-		add(size);
-		add(degree);
+		add(floors);
+		add(rows);
+		add(places);
+		add(abboplekken);
 		add(init);
-		size.setBounds(10, 10, 70, 30);
-		degree.setBounds(10, 50, 70, 30);
-		init.setBounds(10, 90, 70, 30);
+		floors.setBounds(10, 10, 70, 30);
+		rows.setBounds(10, 50, 70, 30);
+		places.setBounds(10, 90, 70, 30);
+		abboplekken.setBounds(10, 130, 70, 30);
+		init.setBounds(10, 170, 70, 30);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		setVisible(true);
@@ -35,7 +42,20 @@ public class InitController extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+		int floors = 0;
+		int rows = 0;
+		int places = 0;
+		int abboplekken = 0;
+		
+		System.out.println(" floors = " + floors + " rows = " + rows + " places = " + places + " abboplekken = " + abboplekken);
+		
+		floors = Integer.parseInt(this.floors.getText());
+		rows = Integer.parseInt(this.rows.getText());
+		places = Integer.parseInt(this.places.getText());
+		abboplekken = Integer.parseInt(this.abboplekken.getText());
+		
+		System.out.println(" floors = " + floors + " rows = " + rows + " places = " + places + " abboplekken = " + abboplekken);
 		
 	}
 
